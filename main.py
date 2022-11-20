f455627f427b97b20d0fb2e8ed9041c5304191b8
klego <klego98@gmail.com>
2022-11-20 17:55:47 +0900

    loading scene working

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import QPalette, QMovie
 from PyQt5.uic import loadUi
+from PyQt5 import uic
 from media import CMultiMedia
 import os
 import sys
@@ -14,7 +15,7 @@ from openpyxl.reader.excel import load_workbook
 QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
 
 #from EditAudio import *
-
+FROM_CLASS_Loading = uic.loadUiType("load.ui")[0]
 class CWidget(QWidget):
     file_sender = pyqtSignal(object)
     def __init__(self):
@@ -202,11 +203,14 @@ class ThreadClass(QThread,QWidget):
             load_ws = load_wb['Sheet1']
             maxrow = load_ws.max_row
             # 셀 주소로 값 출력
-
+            #self.loading = loading(self)
             for i in range(2, maxrow + 1):
                 a = load_ws['A' + str(i)].value
                 eng_wav = gTTS(a, lang='ko')
                 eng_wav.save('kor' + str(i) + '.wav')
+            self.loading
+            #self.loading.deleteLater()
+
 
         self._mutex.unlock()
 
@@ -215,6 +219,30 @@ class ThreadClass(QThread,QWidget):
         self._mutex.unlock()
 
 
+
+class loading(QWidget, FROM_CLASS_Loading):
+
+    def __init__(self, parent):
+        super(loading, self).__init__(parent)
+        self.setupUi(self)
+        self.center()
+        self.show()
+
+        # 동적 이미지 추가
+        self.movie = QMovie('loading.gif', QByteArray(), self)
+        self.movie.setCacheMode(QMovie.CacheAll)
+        # QLabel에 동적 이미지 삽입
+        self.label.setMovie(self.movie)
+        self.movie.start()
+        # 윈도우 해더 숨기기
+        self.setWindowFlags(Qt.FramelessWindowHint)
+
+    # 위젯 정중앙 위치
+    def center(self):
+        size = self.size()
+        ph = self.parent().geometry().height()
+        pw = self.parent().geometry().width()
+        self.move(int(pw / 2 - size.width() / 2), int(ph / 2 - size.height() / 2))
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     w = CWidget()
